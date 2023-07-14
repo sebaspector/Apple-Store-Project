3cdf5b6419c2c7619b2a09ce03c2761aba5b9a84
sebaspector <94724750+sebaspector@users.noreply.github.com>
2023-07-14 16:35:13 -0300

    Update AppleStore Project.sql

--- AppleStore Project.sql
+++ AppleStore Project.sql
@@ -1,4 +1,3 @@
---Dividí el archivo original en 4 por tamaño, creo tabla combinada de los 4.
 CREATE TABLE Apple_Store_combined as
 
 SELECT * FROM appleStore_description1
@@ -15,11 +14,11 @@ UNION ALL
 
 SELECT * FROM appleStore_description4
 
---Checkeo que ambas tengan las mismas filas (AppleStore tenía dos filas con missing values que eliminé)
+--Check that both files contain same amount of rows (AppleStore had 2 rows with missing values that I deleted)
 SELECT COUNT(*) FROM Apple_Store_combined
 SELECT COUNT(*) FROM AppleStore
 
---Qué tipo de aplicación crear?
+--What type of app to create?
 --EDA (Exploratory Data Analysis)
 select
 	prime_genre,
@@ -27,18 +26,18 @@ select
 from AppleStore
 group by prime_genre
 
---Checkeo el numero de aplicaciones únicas
+--Check nª of unique apps
 
 SELECT COUNT(DISTINCT id) as UniqueApp_ID
 from AppleStore
 
---Checkeo missing values
+--Missing values
 
 SELECT COUNT(*) as Missing_values
 from AppleStore
 where track_name is null or user_rating is null or prime_genre is null
 
---Número de apps por género
+--Nª of apps by genre
 
 select 
 	prime_genre,
@@ -47,7 +46,7 @@ from AppleStore
 group by prime_genre
 order by num_apps desc
 
---Overview de ratings
+--Ratings overview
 
 select 
 	min(user_rating),
@@ -56,8 +55,7 @@ select
 from AppleStore
 
 --Data Analysis
---Aplicaciones pagas tienen mas ranking que aplicaciones no pagas (en promedio, las aplicaciones pagas tienen rating
---de 0.35 más alto que las no pagas)
+--Paid applications have higher rankings than free applications (on average, paid applications have a rating that is 0.35 higher than free applications).
 SELECT
 	CASE WHEN PRICE>0 THEN 'Aplicación_paga'
     else 'Aplicacion_no_paga'
@@ -66,7 +64,7 @@ SELECT
 from AppleStore
 group by Tipo_aplicación
 
---Checkeo si aplicaciones con mas lenguajes tienen más rating (en promedio, entre 10 y 30 lenguajes tiene el rating más alto)
+--Apps with more languages have higher ratings?. On average, applications that support between 10 and 30 languages have the highest rating.
 
 SELECT
 	CASE WHEN lang_num < 10 THEN '<10 languages'
@@ -78,7 +76,7 @@ from AppleStore
 group by languages_bucket
 order by avg(user_rating) desc
 
---Checkeo géneros con bajo rating
+--Genres with low rating
 
 SELECT
 	prime_genre,
@@ -88,7 +86,7 @@ group by prime_genre
 ORDER BY avg(user_rating)
 LIMIT 10
 
---Correlation App description length y user_rating (más larga descripción, mayor rating en promedio)
+--Correlation between App description length y user_rating (the longer the description, the higher its rating, in average)
 select
 	case when length(app_desc) < 500 then 'Short'
     when length(app_desc) between 500 and 1000 then 'Medium'
@@ -100,7 +98,7 @@ join Apple_Store_combined b on a.id = b.id
 group by description_length_bucket
 order by avg(user_rating) desc
 
---Checkeo apps mejor rankeadas por género
+--Apps best ranked by genre
 
 select
 	prime_genre,
@@ -114,15 +112,13 @@ from (SELECT
       from AppleStore) a
 where ranking = 1 
 
---Recomendaciones
---1) Aplicacion paga tiene mas rating que la gratuita en promedio, usuarios que pagan apps tienen mayor 
---engagement y encuentran más valor, llevando a mayor rating
---2) Aplicaciones que tienen entre 10 y 30 lenguajes tienen el mayor rating, es mejor enfocarse en algunos lenguajes
---que tener mas de 30.
---3) Categorias como Catalogue, Finance y Book tienen menores ratings, crear aplicaciones de calidad en estas
--- categorías hay potencial para mejores ratings y penetración.
---4) La longitud de la descripción de la aplicación correlaciona positivamente con mayor rating
---5) Nuevas aplicaciones deberían apuntar a tener rating > 3.5, que es el promedio actual del mercado
---6) Games y Enternainment tienen alto número de aplicaciones, lo cual indica saturación de esos mercados
+--Recommendations--
+
+--1) Paid applications have higher ratings on average compared to free applications. Users who pay for apps tend to have higher engagement and find more value, leading to higher ratings.
+--2) Applications that support between 10 and 30 languages have the highest ratings. It is better to focus on a select few languages rather than supporting more than 30.
+--3) Categories such as Catalogue, Finance, and Book have lower ratings. Creating high-quality applications in these categories presents an opportunity for better ratings and market penetration.
+--4) The length of the application description positively correlates with higher ratings.
+--5) New applications should aim for a rating higher than 3.5, which is the current market average.
+--6) The Games and Entertainment categories have a high number of applications, indicating market saturation in those areas.
       
       
